ddce026a49b8d66cb668e6dc1c35443f9bc69c75
nisalrenuja <nrenuja@gmail.com>
2022-01-11 02:59:52 +0530

    Provinces added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,9 @@
-export const provinces : string[] = ['west', 'east']
-export const districts : string[] = []
-export const cities : string[] = []
+export const provinces: string[] = [
+  "Central",
+  "Eastern",
+  "Northern",
+  "Southern",
+  "Western",
+];
+export const districts: string[] = [""];
+export const cities: string[] = [];
